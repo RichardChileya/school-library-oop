4042cd1828c4631b26ec3c48c7afb11c78a9f042
RichardChileya <richardchileyawk@gmail.com>
2022-12-20 23:09:20 +0200

    Add test file

---
+++ test.rb
@@ -0,0 +1,12 @@
+require './person'
+require './capitalize_decorator'
+require './trimmer_decorator'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
+capitalized_trimmed_person.correct_name
+
+#puts person.correct_name
