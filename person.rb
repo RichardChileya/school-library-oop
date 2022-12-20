6b407c83ab9d9b7e4cd6139d6e17bf622c318d0f
RichardChileya <richardchileyawk@gmail.com>
2022-12-20 23:10:00 +0200

    update person class link person to nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,10 @@
-class Person
+require_relative './nameable.rb'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'unknown', parrent_permission: true)
+  def initialize(age, name: 'unknown', parrent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
