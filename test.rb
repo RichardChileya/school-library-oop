3aa07ba7dceaaab2b3fe84dc9c85ab4c6c193eb2
RichardChileya <richardchileyawk@gmail.com>
2022-12-23 00:31:20 +0200

    update test file

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
-require './person'
-require './capitalize_decorator'
-require './trimmer_decorator'
+require_relative './person'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
@@ -9,4 +9,4 @@ capitalized_person.correct_name
 capitalized_trimmed_person = TrimmerDecorator.new(capitalizedPerson)
 capitalized_trimmed_person.correct_name
 
-# puts person.correct_name
+puts person.correct_name
