b6264889e994f8c6e5ab89408a1efbd502daaa05
RichardChileya <richardchileyawk@gmail.com>
2022-12-24 09:47:28 +0200

    Add accessors

--- person.rb
+++ person.rb
@@ -2,16 +2,16 @@ require_relative './nameable'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :rental
+  attr_accessor :name, :age, :rentals
 
-  def initialize(age, name: 'unknown', parrent_permission: true)
+  def initialize(age, name = 'unknown', parrent_permission = 'true')
     super()
 
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parrent_permission = parrent_permission
-    @rental = []
+    @rentals = []
   end
 
   def can_use_service?
@@ -26,8 +26,8 @@ class Person < Nameable
     @name
   end
 
-  def rentals(rental)
-    @rental << rental
+  def add_rental(rental)
+    @rentals.push
     rental.person = self
   end
 end
